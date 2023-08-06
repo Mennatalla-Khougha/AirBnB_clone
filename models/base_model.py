1525bb1ef72f7d52b919759c3541eb8e2e4c83ba
George Raafat <georgexraafat@gmail.com>
2023-08-06 13:51:31 -0700

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
 # This is the BaseModel class
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel():
     """Base model for all models"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialization of BaseModel class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    date = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, date)
+                elif key == "__class__":
+                    pass
+                else:
+                    setattr(self, key, value)
 
     def __str__(self):
         """Return string representation of the class"""
@@ -18,7 +28,7 @@ class BaseModel():
 
     def save(self):
         """save the Updated date"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """Return a dictionary of the class"""
